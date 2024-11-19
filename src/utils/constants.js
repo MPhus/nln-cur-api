e589be01aa9d9cb064e15d797e6652481dbbc618
MPhus <phu3009@gmail.com>
2024-11-19 20:39:10 +0700

    Change ENV

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,8 @@
 import { env } from '~/config/environment'
-const PUBLIC_HOST = 'https://b4df-2402-800-6343-34d1-ec61-4205-5ad1-4792.ngrok-free.app'
 export const WHITELIST_DOMAIN = [
 	'http://localhost:5173',
 	'http://localhost:3000/v1/web/verifyMail',
-	PUBLIC_HOST
+	env.PUBLIC_HOST
 ]
 export const PRODUCT_TYPE = {
 	TOP: 'top',
