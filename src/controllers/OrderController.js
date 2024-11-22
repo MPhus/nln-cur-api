0c81dd1ca083fd83f6747eeb847009a51945dcd8
MPhus <phu3009@gmail.com>
2024-11-22 14:08:02 +0700

    Fix zaloPay callback

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -212,7 +212,7 @@ const getPaymentPage = async (req, res, next) => {
 			amount: (dataSaveDatabase.totalPrice + dataSaveDatabase.tranportFee) * 1000,
 			description: `TiemCUR_Payment for the order #${transID}`,
 			bank_code: '',
-			callback_url: 'https://b4df-2402-800-6343-34d1-ec61-4205-5ad1-4792.ngrok-free.app/v1/web/tiemcur/order/zaloCallback'
+			callback_url: `${env.PUBLIC_HOST}/v1/web/tiemcur/order/zaloCallback`
 		};
 		const data = order.app_id + '|' + order.app_trans_id + '|' + order.app_user + '|' + order.amount + '|' + order.app_time + '|' + order.embed_data + '|' + order.item;
 		order.mac = CryptoJS.HmacSHA256(data, ZALOPAY_CONFIG.key1).toString()
